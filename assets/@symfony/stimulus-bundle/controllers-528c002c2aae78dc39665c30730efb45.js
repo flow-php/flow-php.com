dfdae265e0bb7135f7a5c906deb847c8ac23e2eb
flow-php <>
2024-02-05 10:14:51 +0000

    Update from https://github.com/flow-php/flow/commit/ef659848049dd824d7f23173d6b2f84871d352b1

---
+++ assets/@symfony/stimulus-bundle/controllers-528c002c2aae78dc39665c30730efb45.js
@@ -0,0 +1,5 @@
+import controller_0 from "../../controllers/clipboard_controller.js";
+import controller_1 from "../../controllers/syntax_highlight_controller.js";
+export const eagerControllers = {"clipboard": controller_0, "syntax-highlight": controller_1};
+export const lazyControllers = {};
+export const isApplicationDebug = false;
