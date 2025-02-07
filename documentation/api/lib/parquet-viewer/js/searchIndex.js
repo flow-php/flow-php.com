43c34bff658b457dd54cae56aa30f8e67601d64d
flow-php <>
2025-02-07 20:45:33 +0000

    Update from https://github.com/flow-php/flow/commit/add4392aa8c73aa04117a5322da38fbefedd2671

---
+++ documentation/api/lib/parquet-viewer/js/searchIndex.js
@@ -0,0 +1,44 @@
+Search.appendIndex(
+    [
+                {
+            "fqsen": "\\Flow\\ParquetViewer\\Command\\ReadDataCommand",
+            "name": "ReadDataCommand",
+            "summary": "",
+            "url": "classes/Flow-ParquetViewer-Command-ReadDataCommand.html"
+        },                {
+            "fqsen": "\\Flow\\ParquetViewer\\Command\\ReadDDLCommand",
+            "name": "ReadDDLCommand",
+            "summary": "",
+            "url": "classes/Flow-ParquetViewer-Command-ReadDDLCommand.html"
+        },                {
+            "fqsen": "\\Flow\\ParquetViewer\\Command\\ReadMetadataCommand",
+            "name": "ReadMetadataCommand",
+            "summary": "",
+            "url": "classes/Flow-ParquetViewer-Command-ReadMetadataCommand.html"
+        },                {
+            "fqsen": "\\Flow\\ParquetViewer\\Parquet",
+            "name": "Parquet",
+            "summary": "",
+            "url": "classes/Flow-ParquetViewer-Parquet.html"
+        },                {
+            "fqsen": "\\",
+            "name": "\\",
+            "summary": "",
+            "url": "namespaces/default.html"
+        },                {
+            "fqsen": "\\Flow\\ParquetViewer\\Command",
+            "name": "Command",
+            "summary": "",
+            "url": "namespaces/flow-parquetviewer-command.html"
+        },                {
+            "fqsen": "\\Flow\\ParquetViewer",
+            "name": "ParquetViewer",
+            "summary": "",
+            "url": "namespaces/flow-parquetviewer.html"
+        },                {
+            "fqsen": "\\Flow",
+            "name": "Flow",
+            "summary": "",
+            "url": "namespaces/flow.html"
+        }            ]
+);
