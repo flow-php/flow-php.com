e0ea71f953e375a8e7a5a40746f98db067dc21d2
flow-php <>
2025-02-12 22:00:40 +0000

    Update from https://github.com/flow-php/flow/commit/86a2b4407e763b46f0c63c5b33e40dfb19753813

--- documentation/api/core/js/searchIndex.js
+++ documentation/api/core/js/searchIndex.js
@@ -4036,6 +4036,26 @@ Search.appendIndex(
             "summary": "",
             "url": "classes/Flow-ETL-Function-Hash.html#method_eval"
         },                {
+            "fqsen": "\\Flow\\ETL\\Function\\IndexOf",
+            "name": "IndexOf",
+            "summary": "",
+            "url": "classes/Flow-ETL-Function-IndexOf.html"
+        },                {
+            "fqsen": "\\Flow\\ETL\\Function\\IndexOf\u003A\u003A__construct\u0028\u0029",
+            "name": "__construct",
+            "summary": "",
+            "url": "classes/Flow-ETL-Function-IndexOf.html#method___construct"
+        },                {
+            "fqsen": "\\Flow\\ETL\\Function\\IndexOf\u003A\u003Aeval\u0028\u0029",
+            "name": "eval",
+            "summary": "",
+            "url": "classes/Flow-ETL-Function-IndexOf.html#method_eval"
+        },                {
+            "fqsen": "\\Flow\\ETL\\Function\\IndexOf\u003A\u003Areturns\u0028\u0029",
+            "name": "returns",
+            "summary": "Defines\u0020the\u0020return\u0020type\u0020of\u0020the\u0020function\u0020which\u0020can\u0020be\u0020used\u0020to\ncreate\u0020a\u0020type\u002Dsafe\u0020entry\u0020even\u0020from\u0020non\u0020deterministic\u0020values.",
+            "url": "classes/Flow-ETL-Function-IndexOf.html#method_returns"
+        },                {
             "fqsen": "\\Flow\\ETL\\Function\\IsIn",
             "name": "IsIn",
             "summary": "",
@@ -4995,6 +5015,11 @@ Search.appendIndex(
             "name": "hash",
             "summary": "",
             "url": "classes/Flow-ETL-Function-ScalarFunctionChain.html#method_hash"
+        },                {
+            "fqsen": "\\Flow\\ETL\\Function\\ScalarFunctionChain\u003A\u003AindexOf\u0028\u0029",
+            "name": "indexOf",
+            "summary": "",
+            "url": "classes/Flow-ETL-Function-ScalarFunctionChain.html#method_indexOf"
         },                {
             "fqsen": "\\Flow\\ETL\\Function\\ScalarFunctionChain\u003A\u003AisEven\u0028\u0029",
             "name": "isEven",
