459d5190137f0f911621a052f196e4ab0a47e232
flow-php <>
2025-05-16 17:58:13 +0000

    Update from https://github.com/flow-php/flow/commit/e44b3124cdfe431705e643227aae8b3e5d169e05

---
+++ documentation/api/lib/snappy/js/searchIndex.js
@@ -0,0 +1,39 @@
+Search.appendIndex(
+    [
+                {
+            "fqsen": "\\Flow\\Snappy\\Snappy",
+            "name": "Snappy",
+            "summary": "",
+            "url": "classes/Flow-Snappy-Snappy.html"
+        },                {
+            "fqsen": "\\Flow\\Snappy\\Snappy\u003A\u003A__construct\u0028\u0029",
+            "name": "__construct",
+            "summary": "",
+            "url": "classes/Flow-Snappy-Snappy.html#method___construct"
+        },                {
+            "fqsen": "\\Flow\\Snappy\\Snappy\u003A\u003Acompress\u0028\u0029",
+            "name": "compress",
+            "summary": "",
+            "url": "classes/Flow-Snappy-Snappy.html#method_compress"
+        },                {
+            "fqsen": "\\Flow\\Snappy\\Snappy\u003A\u003Auncompress\u0028\u0029",
+            "name": "uncompress",
+            "summary": "",
+            "url": "classes/Flow-Snappy-Snappy.html#method_uncompress"
+        },                {
+            "fqsen": "\\",
+            "name": "\\",
+            "summary": "",
+            "url": "namespaces/default.html"
+        },                {
+            "fqsen": "\\Flow\\Snappy",
+            "name": "Snappy",
+            "summary": "",
+            "url": "namespaces/flow-snappy.html"
+        },                {
+            "fqsen": "\\Flow",
+            "name": "Flow",
+            "summary": "",
+            "url": "namespaces/flow.html"
+        }            ]
+);
