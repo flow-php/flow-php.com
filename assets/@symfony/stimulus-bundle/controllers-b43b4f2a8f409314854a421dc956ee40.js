7f1436c62a1587bff92b0fa01125090a04eb3d25
flow-php <>
2025-08-30 16:34:32 +0000

    Update from https://github.com/flow-php/flow/commit/dc08f103d1875eb7dece7bae942f16c92249af38

--- assets/@symfony/stimulus-bundle/controllers-b43b4f2a8f409314854a421dc956ee40.js
+++
@@ -1,8 +0,0 @@
-import controller_0 from "../../controllers/all_links_external_controller.js";
-import controller_1 from "../../controllers/clipboard_controller.js";
-import controller_2 from "../../controllers/mermaid_controller.js";
-import controller_3 from "../../controllers/syntax_highlight_controller.js";
-import controller_4 from "../../controllers/scroll_to_top_controller.js";
-export const eagerControllers = {"all-links-external": controller_0, "clipboard": controller_1, "mermaid": controller_2, "syntax-highlight": controller_3, "scroll-to-top": controller_4};
-export const lazyControllers = {};
-export const isApplicationDebug = false;
